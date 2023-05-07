7ae3002326993dbb624f7b3c4f55beafaa245a58
punky11 <shernande998@soy.sena.edu.co>
2023-05-07 11:56:04 -0500

    ruta

--- config/connectionmongoose.js
+++ config/connectionmongoose.js
@@ -3,4 +3,9 @@ const mongoose = require('mongoose')
 const uri ='mongodb+srv://LauraAdso:ZGi3b7V9f1CNADek@cluster0.idychtf.mongodb.net/Mascotas?retryWrites=true&w=majority';
 mongoose.connect(uri, { useNewUrlParser: true });
 
+const parametros = {
+    usuario: process.env.DBUSER,
+    mipass: process.env.DBPASSWORD,
+    mibd: process.env.DBNAME
+}
 module.exports= mongoose
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 
-const PORT = process.env.PORT || 9200 //Revisa el puerto o la variable de entorno asociada al proceso de node en el sistema operativo
+const dotenv = require('dotenv');
+dotenv.config()
+const PORT = process.env.PORT //Revisa el puerto o la variable de entorno asociada al proceso de node en el sistema operativo
 const path = require('path');
 const app = express()
-const mascota = require('./models/mascota')
+const Mascota = require('./models/mascota')
+const router = require('router')
+const routercito = require('./routes/enrutamiento');
+app.use('/api/v1/', routercito)
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -12,31 +17,6 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.render('index');
 })
-
-app.get('/mascotas', async(req, res)=> {
-    let listaMascotas = await mascota.find({});
-    console.log(listaMascotas)
-    res.render('mascotas.ejs', {
-        "nombre": listaMascotas
-    })
-})
-
-app.post('/mascotas', async(req, res)=> { //Se define el método.
-    const nombremascota = req.body.Name;
-    const razamascota = req.body.Raza;
-    const idmascotica = req.body.Id //Se crea una constante que almacene el nombre que se puso en el input con el name: Name.
-    const mascotica = new mascota({ //Se define la constante que almacena el modelo establecido en models. En caso de necesitar mas valores se agregan aquí.
-        Name: nombremascota,
-        raza: razamascota,
-        id: idmascotica//Clave valor
-    })
-    await mascotica.save() //la mascotica será guardada
-    res.send(`La mascota ${mascotica} se registró con éxito!!`) //Mensaje al guardar correctamente
-})
-
-app.put('/actualizarmascota/:id')
-
-
 app.listen(PORT, () => {
     console.log('estoy en línea en el puerto: ' + PORT)
 }
